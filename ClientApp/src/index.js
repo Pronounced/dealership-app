8b3af89f2956c1d9725ad181e1e87cc2111019f7
Fang He <fanghe3@yahoo.com>
2021-03-29 13:45:46 -0400

    add message fn

--- ClientApp/src/index.js
+++ ClientApp/src/index.js
@@ -7,7 +7,7 @@ import registerServiceWorker from './registerServiceWorker';
 
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
-const connection = "localhost";
+const connection = "https://localhost";
 
 ReactDOM.render(
   <BrowserRouter basename={baseUrl}>
